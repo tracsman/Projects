c06aba032b99eb4c1ad6ea86950b5ead7a212e4a
Jon Ormond <jonor@microsoft.com>
2019-09-19 21:03:11 -0700

    Add ECX Functions to LabMod

--- LabModPS/LabMod/Public/New-LabECX.ps1
+++ LabModPS/LabMod/Public/New-LabECX.ps1
@@ -6,40 +6,49 @@
         [int]$TenantID,
         [Parameter(Mandatory=$true, HelpMessage='Enter ER Location')]
         [ValidateSet('Ashburn','Seattle')]
-        [string]$Location,
+        [string]$PeeringLocation,
         [ValidateSet('ExpressRoute-Lab','Pathfinder')]
         [string]$Subscription='ExpressRoute-Lab')
 
-    # Equinix Provisioning Script for the AFD Lab
+    # Equinix Provisioning Script for PathFinder Lab
     #
-    # To ensure you are in the right subscrition run
-    # Get-AzContext
-    #
-    # 1. Get ECX Token
-    # 2. Load all ER circuits into an array
-    # 3. Loop through array
-    # 3.1 If enabled and not provisioned, provision the circuit
-    #
-    # Re-run this script as often as needed during the
-    # class to get the ExpressRoute circuits provisioned.
+    # 1. Initialize
+    # 2. Validate
+    # 3. Get ECX Token
+    # 4. Load all ER circuits into an array
+    # 5. Loop through array
+    # 5.1 If enabled and not provisioned, provision the circuit
+    # 6. End Nicely
     #
 
-    # Az Module Test
-    $ModCheck = Get-Module Az.Network -ListAvailable
-    If ($Null -eq $ModCheck) {
-        Write-Warning "The Az PowerShell module was not found. This script uses the Az modules for PowerShell"
-        Write-Warning "See the blob post for more information at: https://azure.microsoft.com/blog/how-to-migrate-from-azurerm-to-az-in-azure-powershell/"
-        Return
-        }
-
-    # Non-configurable Variable Initialization (ie don't modify these)
+    # 1. Initialize
+    # Script Variables
+    $StartTime = Get-Date
+    $ProvisionCount = 0
     Switch ($Subscription) {
         'ExpressRoute-Lab' {$SubID = '4bffbb15-d414-4874-a2e4-c548c6d45e2a'}
         'Pathfinder' {$SubID = '79573dd5-f6ea-4fdc-a3aa-d05586980843'}
     }
+
+    # Azure Variables
+    If ($PeeringLocation -eq "Seattle") {$RGName = "SEA-Cust$TenantID"}
+    Else {$RGName = "ASH-Cust$TenantID"}
+    $TenantStub = "$RGName-ER"
+    $kvName = "LabSecrets"
+    $kvECXClientID = "ECXClientID"
+    $kvECXSecret = "ECXSecret"
+    
+    # Equinix Variables
     $ConnProfileUUID = "a1390b22-bbe0-4e93-ad37-85beef9d254d"
-    If ($Location -eq "Seattle") {$TenantStub = "SEA-Cust$TenantID-ER"}
-    Else {$TenantStub = "ASH-Cust$TenantID-ER"}
+
+    # 2. Validate
+    # Az Module Test
+    $ModCheck = Get-Module Az.Network -ListAvailable
+    If ($Null -eq $ModCheck) {
+        Write-Warning "The Az PowerShell module was not found. This commandlet uses the Az modules for PowerShell"
+        Write-Warning "See the blob post for more information at: https://azure.microsoft.com/blog/how-to-migrate-from-azurerm-to-az-in-azure-powershell/"
+        Return
+        }
 
     # Login and permissions check
     Write-Host (Get-Date)' - ' -NoNewline
@@ -56,15 +65,15 @@
                     Return}
     }
 
-    # 1. Get ECX Token
-    $kvName = "LabSecrets"
-    $kvECXClientID = "ECXClientID"
-    $kvECXSecret = "ECXSecret"
-    Write-Host "  Grabbing ECX secrets"
+    # 3. Get ECX Token
+    Write-Host (Get-Date)' - ' -NoNewline
+    Write-Host "Getting ECX OAuth Token" -ForegroundColor Cyan
+    Write-Host "  Grabbing ECX secrets..." -NoNewline
     $ECXClientID = Get-AzKeyVaultSecret -VaultName $kvName -Name $kvECXClientID
     $ECXSecret = Get-AzKeyVaultSecret -VaultName $kvName -Name $kvECXSecret -ErrorAction Stop
 
     # Get REST OAuth Token
+    Write-Host "  Getting OAuth Token"
     $TokenURI = "https://api.equinix.com/oauth2/v1/token"
     $TokenBody = "{" + 
                 "  ""grant_type"": ""client_credentials""," +
@@ -72,21 +81,24 @@
                 "  ""client_secret"": ""$ECXSecret""" +
                 "}"
     Try {$token = Invoke-RestMethod -Method Post -Uri $TokenURI -Body $TokenBody -ContentType application/json}
-    Catch {Write-Warning "An error occured getting an OAuth certificate from Equinix."
+    Catch {Write-Host
+        Write-Warning "An error occured getting an OAuth certificate from Equinix."
         Write-Host
         Write-Host $error[0].ErrorDetails.Message
         Return }
     $ConnHeader =  @{"Authorization" = "Bearer $($token.access_token)"}
+    Write-Host "Sucess" -ForegroundColor Green
 
-    # 2. Load all ER circuits into an array
-    $Circuits = Get-AzExpressRouteCircuit | Where-Object Name -Like "$TenantStub*"
+    # 4. Load all ER circuits into an array
+    Write-Host (Get-Date)' - ' -NoNewline
+    Write-Host "Pulling ER Circuit(s)" -ForegroundColor Cyan
+    $Circuits = Get-AzExpressRouteCircuit -ResourceGroupName $RGName | Where-Object Name -Like "$TenantStub*"
 
-    # 3. Loop through array
-    # Provision Circuit(s)
+    # 5. Loop through Circuit array
     ForEach ($Circuit in $Circuits | Sort-Object ResourceGroupName ) {
         If ($Circuit.CircuitProvisioningState -eq "Enabled" -and $Circuit.ServiceProviderProvisioningState -eq "NotProvisioned") {
-            # 3.1 If enabled and not provisioned, provision the circuit
-            If ($Location -eq "Ashburn") {
+            # 5.1 If enabled and not provisioned, provision the circuit
+            If ($PeeringLocation -eq "Ashburn") {
                 $ConnPriPortUUID = "66284add-88dd-8dd0-b4e0-30ac094f8af1"
                 $ConnSecPortUUID = "66284add-88df-8df0-b4e0-30ac094f8af1"
                 $ConnMetro = "DC"
@@ -123,7 +135,8 @@
             If ($connection.message -eq "Connection Saved Successfully") {
                 Write-Host (Get-Date)' - ' -NoNewline
                 Write-Host $Circuit.Name -NoNewline -ForegroundColor Yellow
-                Write-Host " has been provisioned"}
+                Write-Host " has been provisioned"
+                $ProvisionCount++}
             Else {Write-Warning $Circuit.Name "provisioing has failed"} 
         }
         ElseIf ($Circuit.ServiceProviderProvisioningState -eq "Provisioned") {
@@ -142,4 +155,15 @@
             Write-Host " is in an unknown state"
         }
     }
+
+    # 6. End nicely
+    $EndTime = Get-Date
+    $TimeDiff = New-TimeSpan $StartTime $EndTime
+    $Mins = $TimeDiff.Minutes
+    $Secs = $TimeDiff.Seconds
+    $RunTime = '{0:00}:{1:00} (M:S)' -f $Mins,$Secs
+    Write-Host (Get-Date)' - ' -NoNewline
+    Write-Host "$ProvisionCount circuits provisioned ($($ProvisionCount*2) ECX connections)" -ForegroundColor Green
+    Write-Host "Time to create: $RunTime"
+    Write-Host
 }
--- LabModPS/LabMod/Public/Remove-LabECX.ps1
+++ LabModPS/LabMod/Public/Remove-LabECX.ps1
@@ -6,24 +6,39 @@
         [int]$TenantID,
         [Parameter(Mandatory=$true, HelpMessage='Enter ER Location')]
         [ValidateSet('Ashburn','Seattle')]
-        [string]$Location,
+        [string]$PeeringLocation,
         [ValidateSet('ExpressRoute-Lab','Pathfinder')]
         [string]$Subscription='ExpressRoute-Lab')
 
-    # Equinix Provisioning Script for the AFD Lab
+    # Equinix Provisioning Script for PathFinder Lab
     #
-    # To ensure you are in the right subscrition run
-    # Get-AzContext
-    #
-    # 1. Get ECX Token
-    # 2. Load all ER circuits into an array
-    # 3. Loop through array
-    # 3.1 If enabled and not provisioned, provision the circuit
-    #
-    # Re-run this script as often as needed during the
-    # class to get the ExpressRoute circuits provisioned.
+    # 1. Initialize
+    # 2. Validate
+    # 3. Get ECX Token
+    # 4. Load all ER circuits into an array
+    # 5. Load all ECX connections into an array
+    # 6. Loop through Circuits array searching connection array
+    # 7. End Nicely
     #
 
+    # 1. Initialize
+    # Script Variables
+    $StartTime = Get-Date
+    $DeprovisionCount = 0
+    Switch ($Subscription) {
+        'ExpressRoute-Lab' {$SubID = '4bffbb15-d414-4874-a2e4-c548c6d45e2a'}
+        'Pathfinder' {$SubID = '79573dd5-f6ea-4fdc-a3aa-d05586980843'}
+    }
+
+    # Azure Variables
+    If ($PeeringLocation -eq "Seattle") {$RGName = "SEA-Cust$TenantID"}
+    Else {$RGName = "ASH-Cust$TenantID"}
+    $TenantStub = "$RGName-ER"
+    $kvName = "LabSecrets"
+    $kvECXClientID = "ECXClientID"
+    $kvECXSecret = "ECXSecret"
+
+    # 2. Validate
     # Az Module Test
     $ModCheck = Get-Module Az.Network -ListAvailable
     If ($Null -eq $ModCheck) {
@@ -31,14 +46,6 @@
         Write-Warning "See the blob post for more information at: https://azure.microsoft.com/blog/how-to-migrate-from-azurerm-to-az-in-azure-powershell/"
         Return
         }
-
-    # Non-configurable Variable Initialization (ie don't modify these)
-    Switch ($Subscription) {
-        'ExpressRoute-Lab' {$SubID = '4bffbb15-d414-4874-a2e4-c548c6d45e2a'}
-        'Pathfinder' {$SubID = '79573dd5-f6ea-4fdc-a3aa-d05586980843'}
-    }
-    If ($Location -eq "Seattle") {$TenantStub = "SEA-Cust$TenantID-ER"}
-    Else {$TenantStub = "ASH-Cust$TenantID-ER"}
 
     # Login and permissions check
     Write-Host (Get-Date)' - ' -NoNewline
@@ -55,15 +62,15 @@
                     Return}
     }
 
-    # 1. Get ECX Token
-    $kvName = "LabSecrets"
-    $kvECXClientID = "ECXClientID"
-    $kvECXSecret = "ECXSecret"
-    Write-Host "  Grabbing ECX secrets"
+    # 3. Get ECX Token
+    Write-Host (Get-Date)' - ' -NoNewline
+    Write-Host "Getting ECX OAuth Token" -ForegroundColor Cyan
+    Write-Host "  Grabbing ECX secrets..." -NoNewline
     $ECXClientID = Get-AzKeyVaultSecret -VaultName $kvName -Name $kvECXClientID
     $ECXSecret = Get-AzKeyVaultSecret -VaultName $kvName -Name $kvECXSecret -ErrorAction Stop
 
     # Get REST OAuth Token
+    Write-Host "  Getting OAuth Token"
     $TokenURI = "https://api.equinix.com/oauth2/v1/token"
     $TokenBody = "{" + 
                 "  ""grant_type"": ""client_credentials""," +
@@ -71,18 +78,20 @@
                 "  ""client_secret"": ""$ECXSecret""" +
                 "}"
     Try {$token = Invoke-RestMethod -Method Post -Uri $TokenURI -Body $TokenBody -ContentType application/json}
-    Catch {Write-Warning "An error occured getting an OAuth certificate from Equinix."
+    Catch {Write-Host
+        Write-Warning "An error occured getting an OAuth certificate from Equinix."
         Write-Host
         Write-Host $error[0].ErrorDetails.Message
         Return }
     $ConnHeader =  @{"Authorization" = "Bearer $($token.access_token)"}
+    Write-Host "Sucess" -ForegroundColor Green
 
-    # 2. Load all ER circuits into an array
+    # 4. Load all ER circuits into an array
+    Write-Host (Get-Date)' - ' -NoNewline
+    Write-Host "Pulling ER Circuit(s)" -ForegroundColor Cyan
     $Circuits = Get-AzExpressRouteCircuit | Where-Object Name -Like "$TenantStub*"
 
-    # 3. Loop through array
-    # Deprovision Circuit(s)
-    # Load all ECX connections into an array
+    # 5. Load all ECX connections into an array
     $ConnURI = "https://api.equinix.com/ecx/v3/l2/buyer/connections?pageSize=300&status=PROVISIONED"
     Try {$connections = Invoke-RestMethod -Method Get -Uri $ConnURI -Headers $ConnHeader -ErrorAction Stop
         Write-Host "Pulled (up to) top 300 provisioned ECX Connections"}
@@ -91,21 +100,22 @@
         Write-Host $error[0].ErrorDetails.Message
         Return }
     
-    # Loop Circuits and delete
+    # 6. Loop through Circuits array searching connection array
     $ConnURI = "https://api.equinix.com/ecx/v3/l2/connections"
     ForEach ($Circuit in $Circuits | Sort-Object ResourceGroupName ) {
-        If ($Circuit.CircuitProvisioningState -eq "Enabled" -and $Circuit.ServiceProviderProvisioningState -eq "Provisioned") {
-            # 4.1 If provisioned, search for ECX Connection in ECX array
+        # If provisioned, search for ECX Connection in ECX array
+        If ($Circuit.CircuitProvisioningState -eq "Enabled" -and $Circuit.ServiceProviderProvisioningState -eq "Provisioned") { 
             $UUIDs = $connections.content | Where-Object authorizationKey -eq $Circuit.ServiceKey | Sort-Object Name | Select-Object uuid, name
     
-            # 4.2 Loop through any connections and deprovision
+            # Loop through connections and deprovision matching s-key
             ForEach ($UUID in $UUIDs) {
                 $ConnUUID = $UUID.uuid
                 $connection = Invoke-RestMethod -Method Delete -Uri $ConnURI"/$ConnUUID" -Headers $ConnHeader
                 If ($connection.message -eq "deleted connection successfully") {
                     Write-Host (Get-Date)' - ' -NoNewline
                     Write-Host $UUID.Name -NoNewline -ForegroundColor Yellow
-                    Write-Host " has been deprovisioned"}
+                    Write-Host " has been deprovisioned"
+                    $DeprovisionCount++}
                 Else {Write-Warning $Circuit.Name "deprovisioing has failed on "$UUID.Name}
                 }
         }
@@ -119,5 +129,16 @@
             Write-Host $Circuit.Name -NoNewline -ForegroundColor Red
             Write-Host " is in an unknown state"
         }
-    }  
+    } 
+
+    # 7. End nicely
+    $EndTime = Get-Date
+    $TimeDiff = New-TimeSpan $StartTime $EndTime
+    $Mins = $TimeDiff.Minutes
+    $Secs = $TimeDiff.Seconds
+    $RunTime = '{0:00}:{1:00} (M:S)' -f $Mins,$Secs
+    Write-Host (Get-Date)' - ' -NoNewline
+    Write-Host "$($DeprovisionCount/2) circuits deprovisioned ($DeprovisionCount ECX Connections)" -ForegroundColor Green
+    Write-Host "Time to deprovision: $RunTime"
+    Write-Host
 }
