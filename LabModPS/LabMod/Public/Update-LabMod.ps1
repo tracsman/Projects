fc5181d0b1f6777bd74236a0c99092e1978118d2
Jon Ormond <jonor@microsoft.com>
2026-04-08 17:51:00 -0500

    Update LabMod installer for request helpers

--- LabModPS/LabMod/Public/Update-LabMod.ps1
+++ LabModPS/LabMod/Public/Update-LabMod.ps1
@@ -4,8 +4,7 @@
         [switch]$Force=$false)
 
     # Admin Session Check
-    If (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        Write-Warning "This script must be run elevated as Administrator!"
+    if (-not (Assert-LabAdminContext -WarnOnly)) {
         Return
     }
     
@@ -16,13 +15,11 @@
         $reader = new-object IO.StreamReader($httpwebresponse.getResponseStream())
         $content = $reader.ReadToEnd()
         $reader.Close()
-        $i=355
-        $OnlineVersion = ""
-        Do {
-            $OnlineVersion = $OnlineVersion + $content[$i]
-            $i=$i+1
+        $versionMatch = [regex]::Match($content, "ModuleVersion\s*=\s*'(?<version>[^']+)'")
+        if (-not $versionMatch.Success) {
+            throw "Unable to determine the online LabMod version from the module manifest."
         }
-        Until ($content[$i+2] -eq "`n")
+        $OnlineVersion = $versionMatch.Groups['version'].Value
 
         # Get Installed Version
         Try {$CurrentVersion = (Get-Module LabMod -ListAvailable).Version.ToString()}
@@ -37,9 +34,13 @@
         $FileName = @()
         $FileName += 'LabMod.psd1'
         $FileName += 'LabMod.psm1'
+        $FileName += 'Private/Assert-LabAdminContext.ps1'
+        $FileName += 'Private/Write-LabLogEntry.ps1'
+        $FileName += 'Private/Write-LabRunStatus.ps1'
         $FileName += 'Public/Copy-ToUbuntu.ps1'
         $FileName += 'Public/Get-LabECX.ps1'
         $FileName += 'Public/New-LabECX.ps1'
+        $FileName += 'Public/Start-LabVmRequest.ps1'
         $FileName += 'Public/New-LabVM.ps1'
         $FileName += 'Public/New-LabVMDrive.ps1'
         $FileName += 'Public/Remove-LabECX.ps1'
@@ -55,8 +56,13 @@
         Write-Host
 
         ForEach ($File in $FileName) {
+            $targetPath = Join-Path $Destination $File
+            $targetDirectory = Split-Path -Path $targetPath -Parent
+            if (-not (Test-Path $targetDirectory)) {
+                New-Item -ItemType Directory -Path $targetDirectory -Force | Out-Null
+            }
             $webClient = new-object System.Net.WebClient
-            $webClient.DownloadFile( $uri + $File, $Destination + $File )
+            $webClient.DownloadFile( $uri + $File, $targetPath )
             Write-Host "Copied successfully:" $File 
         }
 
